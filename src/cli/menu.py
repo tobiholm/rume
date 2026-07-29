83df8ae4a41600b41170b2cc64f2f9f3cb61fdb2
tobiholm <tobiholm@proton.me>
2026-07-29 19:20:15 +0300

    Create menu.py

---
+++ src/cli/menu.py
@@ -0,0 +1,11 @@
+def show_menu():
+
+    print()
+
+    print("1. Configurations")
+    print("2. Create")
+    print("3. Validate")
+    print("4. Export")
+    print("5. Backup")
+    print("6. Statistics")
+    print("7. Exit")
